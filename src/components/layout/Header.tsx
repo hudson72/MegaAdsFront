1e2e8873540129a5d7c893eee2b80984a66046fe
Irek Iwanca <iiwanca@gmail.com>
2022-05-12 14:43:00 +0200

    App structure divided into basic components.

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Btn} from '../common/Btn';
+
+import './Header.css';
+
+export const Header = () => {
+    return (
+        <header>
+            <h1>
+                <strong>Mega </strong> Ogłoszenia
+            </h1>
+            <Btn text='Dodaj ogłoszenie'/>
+            <div className="search">
+                <input type="text"/> <Btn text='Szukaj'/>
+            </div>
+        </header>
+    )
+}
