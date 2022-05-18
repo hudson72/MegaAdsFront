827ad3e5cf774d1c5ed595336e5e4cdab1abbedb
Irek Iwanca <iiwanca@gmail.com>
2022-05-18 10:41:49 +0200

    Added search context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { Header } from './components/layout/Header';
 import { Map } from './components/Map/Map';
+import { SearchContext } from './contexts/search.context';
 
 export const App = () => {
+    const [search, setSearch] = useState('');
+
     return (
-        <>
+        <SearchContext.Provider value={{search, setSearch}}>
             <Header/>
             <Map/>
-        </>
+        </SearchContext.Provider>
     );
-}
+};
